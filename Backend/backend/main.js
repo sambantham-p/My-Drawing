05963c43960a60a5b0e75d9e2596e8709daaea84
sambantham <sambantham@foxsense.io>
2023-10-15 15:20:18 +0530

    changes made

--- Backend/backend/main.js
+++ Backend/backend/main.js
@@ -5,7 +5,7 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
   cors: {
-    origin: ['https://lets-draw-backend.vercel.app'],
+    origin: ['https://lets-draw-backend.vercel.app/'],
     methods: ['GET', 'POST'],
     credentials: true,
   },
