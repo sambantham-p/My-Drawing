526dd19920a58703fbbe819599558a92bfead242
sambantham <sambantham@foxsense.io>
2023-10-15 20:35:33 +0530

    added port in env

--- Backend/backend/main.js
+++ Backend/backend/main.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = 3001;
+const port = process.env.PORT || 5000;
 const cors = require('cors');
 const app = express();
 const dotenv = require('dotenv');
